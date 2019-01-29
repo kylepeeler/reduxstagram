4d0a9a31c4c41930d90b816ec447d51b136a5cb2
Kyle Peeler <kylepeeler@gmail.com>
2019-01-28 21:51:56 -0500

    fixed remove comment reducer

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -23,12 +23,11 @@ function postComments(state = [], action){
           user: action.author,
           text: action.comment
         }];
-      case 'REMOVE_COMMENT':
-        console.log('removing a comment');
+    case 'REMOVE_COMMENT':
         return [
           // from the start to the one we want to delete
-          ...state.slice(0, action.i),
-          ...state.slice(action.i + 1)
+          ...state.slice(0, action.commentIndex),
+          ...state.slice(action.commentIndex + 1)
         ]
       default:
         return state;
