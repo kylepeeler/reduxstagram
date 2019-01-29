bf5546c51ce8eb4994a669430eb54f27aa365858
Kyle Peeler <kylepeeler@gmail.com>
2019-01-28 21:47:47 -0500

    got posting comments working

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import Photo from './Photo';
-//import Comments from './Comments';
+import Comments from './Comments';
 
 class Single extends Component {
     render() {
         //index of the post
-        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const { postId } = this.props.params
+        const i = this.props.posts.findIndex((post) => post.code === postId);
         const post = this.props.posts[i];
+        const postComments = this.props.comments[postId] || [];
         console.log(post);
         //get us the post
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props} />
+                <Comments postComments={postComments} {...this.props}/>
             </div>
         );
     }
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -4,6 +4,35 @@
 // 2. copy of the current state
 
 function comments(state = [], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take the current this.state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state;
+}
+
+function postComments(state = [], action){
+  switch(action.type){
+    case 'ADD_COMMENT':
+      // return the new state with the new componentDidMount() {
+        return [...state, {
+          user: action.author,
+          text: action.comment
+        }];
+      case 'REMOVE_COMMENT':
+        console.log('removing a comment');
+        return [
+          // from the start to the one we want to delete
+          ...state.slice(0, action.i),
+          ...state.slice(action.i + 1)
+        ]
+      default:
+        return state;
+    }
   return state;
 }
 
