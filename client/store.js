5be1d0c929a063f83a2e365a27fcedc3616d6cbe
Kyle Peeler <kylepeeler@gmail.com>
2019-01-28 23:09:20 -0500

    added redux dev tools

--- client/store.js
+++ client/store.js
@@ -2,6 +2,7 @@ import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
+
 // import the root reducer
 import rootReducer from './reducers/index';
 
@@ -14,7 +15,11 @@ const defaultState = {
     comments
 }
 
-const store = createStore(rootReducer, defaultState);
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : (f) => f
+)
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
